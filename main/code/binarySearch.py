8c45173db6907ae2a06985d677d3ba6f98667878
YuriLigard <yuriligard@hotmail.com>
2022-11-20 18:38:54 -0300

    org alg binary

--- main/code/binarySearch.py
+++ main/code/binarySearch.py
@@ -1,38 +1,49 @@
 from __future__ import annotations
 
+class Binary:
 
-def binary_search(sorted_collection: list[int], item: int = 6) -> int | None:
+    global sorted_collection
+    sorted_collection = [1, 1, 4, 6, 6, 7, 9, 10, 10, 11, 12, 12, 12, 14, 15, 15, 16, 18, 19, 21, 22, 26, 26, 27, 29, 29, 29, 31, 32, 32, 32, 33, 35, 37, 37, 40, 41, 42, 42, 43, 44, 44, 46, 47, 47, 48, 52, 52, 53, 54, 56, 58, 58, 59, 62, 62, 63, 64, 64, 65, 67, 67, 68, 70, 70, 70, 72, 72, 73, 74, 75, 76, 76, 77, 78, 78, 78, 78, 78, 78, 79, 79, 79, 81, 82, 84, 84, 87, 88, 91, 92, 92, 93, 94, 95, 98, 99, 99, 100, 100]
 
-    left = 0
-    right = len(sorted_collection) - 1
+    def __init__(self) -> None:
+        self.item: int = 33
+        ##
 
-    while left <= right:
+    
+    def binary_search(self) -> int | None:
+
+        left = 0
+        right = len(sorted_collection) - 1
+
+        while left <= right:
+            midpoint = left + (right - left) // 2
+            current_item = sorted_collection[midpoint]
+            if current_item == self.item:
+                return midpoint
+            elif self.item < current_item:
+                right = midpoint - 1
+            else:
+                left = midpoint + 1
+        return None
+
+
+    def binary_search_by_recursion(self, left: int = 0, right: int = len(sorted_collection) - 1) -> int | None:
+    
+        if right < left:
+            return None
+    
         midpoint = left + (right - left) // 2
         current_item = sorted_collection[midpoint]
-        if current_item == item:
+        if current_item == self.item:
             return midpoint
-        elif item < current_item:
-            right = midpoint - 1
         else:
-            left = midpoint + 1
-    return None
-
-
-def binary_search_by_recursion(sorted_collection: list[int], item: int, left: int, right: int) -> int | None:
-
-    if right < left:
-        return None
-    
-    midpoint = left + (right - left) // 2
-    current_item = sorted_collection[midpoint]
-    if current_item == item:
-        return midpoint
-    else:
-        if current_item > item:
-           return binary_search_by_recursion(sorted_collection=sorted_collection, item=item, left=left, right=midpoint-1)
-        else:
-           return binary_search_by_recursion(sorted_collection=sorted_collection, item=item, left=midpoint+1, right=right)
+            if current_item > self.item:
+                return __class__.binary_search_by_recursion(self, left=left, right=midpoint-1)
+            else:
+                return __class__.binary_search_by_recursion(self, left=midpoint+1, right=right)
 
 if __name__ == "__main__":
-    sorted_collection = [1, 1, 4, 6, 6, 7, 9, 10, 10, 11, 12, 12, 12, 14, 15, 15, 16, 18, 19, 21, 22, 26, 26, 27, 29, 29, 29, 31, 32, 32, 32, 33, 35, 37, 37, 40, 41, 42, 42, 43, 44, 44, 46, 47, 47, 48, 52, 52, 53, 54, 56, 58, 58, 59, 62, 62, 63, 64, 64, 65, 67, 67, 68, 70, 70, 70, 72, 72, 73, 74, 75, 76, 76, 77, 78, 78, 78, 78, 78, 78, 79, 79, 79, 81, 82, 84, 84, 87, 88, 91, 92, 92, 93, 94, 95, 98, 99, 99, 100, 100]
-    binary_search_by_recursion(sorted_collection = sorted_collection, item=100, left=0, right=(len(sorted_collection) - 1))
+    #sorted_collection = [1, 1, 4, 6, 6, 7, 9, 10, 10, 11, 12, 12, 12, 14, 15, 15, 16, 18, 19, 21, 22, 26, 26, 27, 29, 29, 29, 31, 32, 32, 32, 33, 35, 37, 37, 40, 41, 42, 42, 43, 44, 44, 46, 47, 47, 48, 52, 52, 53, 54, 56, 58, 58, 59, 62, 62, 63, 64, 64, 65, 67, 67, 68, 70, 70, 70, 72, 72, 73, 74, 75, 76, 76, 77, 78, 78, 78, 78, 78, 78, 79, 79, 79, 81, 82, 84, 84, 87, 88, 91, 92, 92, 93, 94, 95, 98, 99, 99, 100, 100]
+    binary = Binary()
+    print(binary.binary_search())
+    print(binary.binary_search_by_recursion())
--- main/measurer.py
+++ main/measurer.py
@@ -5,30 +5,31 @@ import time
 from code import Mcarlo, binary_search, binary_search_by_recursion, quicksort, fibo, Fibonacci
 
 
-def measurer(algoritmo: object, repeticoes: int) -> float:
+def measurer(algoritmo: object, loop: int) -> float:
 
-    comeco = time.thread_time()
-    for _ in range(repeticoes):
+    start = time.thread_time()
+    for _ in range(loop):
         algoritmo()
 
-    return (time.thread_time() - comeco)/repeticoes
+    return (time.thread_time() - start)/loop
 
 
 if __name__ == '__main__':
 
     if sys.argv[1] == "MonteCarlo":
-        tempo = measurer(algoritmo=Mcarlo, repeticoes=int(sys.argv[2]))
+        tm = measurer(algoritmo=Mcarlo, loop=int(sys.argv[2]))
     elif sys.argv[1] == "BuscaBinaria":
-        tempo = measurer(algoritmo=binary_search, repeticoes=int(sys.argv[2]))
+        tm = measurer(algoritmo=binary_search, loop=int(sys.argv[2]))
     elif sys.argv[1] == "BuscaBinariaRecursiva":
-        tempo = measurer(algoritmo=binary_search_by_recursion, repeticoes=int(sys.argv[2]))
+        tm = measurer(algoritmo=binary_search_by_recursion, loop=int(sys.argv[2]))
     elif sys.argv[1] == "QuickSort":
-        pass
+        tm = measurer(algoritmo=quicksort, loop=int(sys.argv[2]))
     elif sys.argv[1] == "SeqFibonacci":
-        pass
+        tm = measurer(algoritmo=fibo, loop=int(sys.argv[2]))
     elif sys.argv[1] == "SeqFibonacciRecursiva":
-        pass
+        fibonacci = Fibonacci()
+        tm = measurer(algoritmo=fibonacci.get, loop=int(sys.argv[2]))
     else:
-        tempo = 0.00
+        tm = 0.00
 
-    print(f"{tempo:.4f}")
+    print(f"{tm:.4f}")
